0c3a9a8c3e1e876e8a115603d7695c76e0426c13
vidit-od <vidit894@gmail.com>
2024-06-07 20:21:47 +0530

    transitions added

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -23,14 +23,37 @@ export default {
       },
       width:{
         "96": "400px",
+        "block": "15px"
+      },
+      translate:{
+        "30": "118.5px"
       },
       animation:{
-        "rotate": "circle 1s linear infinite"
+        "rotate": "circle 1s linear infinite",
+        "logo": "loader 5s linear infinite",
+        "loadbar": "bar 5s linear infinite",
+        "maxscale": "maxs 5s linear"
       },
       keyframes : {
         circle: {
           "0%": { transform: "rotate(0deg)"},
           "100%": { transform: "rotate(360deg)" },
+        },
+        loader:{
+          "0% ": {opacity: "0.5" , scale:"0.98"},
+          "5%": {opacity: "1" , scale: "1"},
+          "10% ": {opacity: "0.5" , scale:'0.98'},
+          "15%": {opacity: "1" ,scale: '1'},
+        },
+        bar:{
+          '0% , 100%' : {width: '20%',left : '0'},
+          '25%': {width:'50%'},
+          '50%': {width:'30%', left: '70%'},
+          '75%': {width:'50%'},
+        },
+        maxs:{
+          '0%':  {scale:"1"},
+          '100%':{scale:"100000"},
         }
       },
       
