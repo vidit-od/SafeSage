60eb270c289d931d470ffc21b9c251661ee36aa7
vidit-od <vidit894@gmail.com>
2024-06-11 18:45:02 +0530

    api/v1/tags/add/bulk route added to bulk add tags

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { UserRouter } from "./user";
 import { BlogRouter } from "./blog";
+import { TagRouter } from "./tag";
 
 export const IndexRouter = new Hono();
 
 IndexRouter.route('/user', UserRouter);
 IndexRouter.route('/blog',BlogRouter);
+IndexRouter.route('/tags',TagRouter);
---
+++ Backend/src/routes/tag.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+import prisma from "../prisma";
+
+export const TagRouter = new Hono();
+
+// input array of stings ; all will be added to tags schema 
+TagRouter.post('/add/bulk',async(c)=>{
+    try{
+        const tags:{
+            tags:string[]
+        } = await c.req.json();
+
+        const all_tags = tags.tags
+        const data_tag = all_tags.map((i)=>{
+            return {
+                name: i
+            }
+        })
+        const response = await prisma.tags.createMany({
+            data : data_tag
+        })
+        return c.json(response);
+    }
+    catch(e){
+        return c.json({msg: "error",error:e});
+    }
+})
